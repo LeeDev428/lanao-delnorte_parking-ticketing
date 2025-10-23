8c4cf02e5b0a470db1b40f619cd70e50e1fb9721
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-23 23:13:18 +0800

    Update app-sidebar.tsx

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,7 +13,7 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { LayoutGrid, Ticket, History, Car } from 'lucide-react';
+import { LayoutGrid, Ticket, History } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -21,11 +21,6 @@ const mainNavItems: NavItem[] = [
         title: 'Dashboard',
         href: dashboard(),
         icon: LayoutGrid,
-    },
-    {
-        title: 'New Ticket',
-        href: '/tickets/create',
-        icon: Car,
     },
     {
         title: 'Active Tickets',
