11628189124e273cee8a9f5d1f83b9257b9e8ef9
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-10-23 20:27:21 +0800

    Update app-sidebar.tsx

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,7 +13,7 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { BookOpen, Folder, LayoutGrid } from 'lucide-react';
+import { LayoutGrid, Ticket, History, Car } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -22,20 +22,24 @@ const mainNavItems: NavItem[] = [
         href: dashboard(),
         icon: LayoutGrid,
     },
+    {
+        title: 'New Ticket',
+        href: '/tickets/create',
+        icon: Car,
+    },
+    {
+        title: 'Active Tickets',
+        href: '/tickets',
+        icon: Ticket,
+    },
+    {
+        title: 'History',
+        href: '/tickets/history',
+        icon: History,
+    },
 ];
 
-const footerNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/react-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#react',
-        icon: BookOpen,
-    },
-];
+const footerNavItems: NavItem[] = [];
 
 export function AppSidebar() {
     return (
