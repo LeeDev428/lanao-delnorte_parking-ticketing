a46ddb0cd18e1414a7f56c2023b52669fa31d939
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-14 14:54:42 +0800

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,27 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lanaodelnorte.parking',
+  appName: 'Parking Ticketing',
+  webDir: 'public',
+  server: {
+    androidScheme: 'http',
+    // For development with local Laravel server:
+    url: 'http://192.168.0.36:8000',
+    cleartext: true
+  },
+  plugins: {
+    SplashScreen: {
+      launchShowDuration: 2000,
+      backgroundColor: "#ffffff",
+      androidSplashResourceName: "splash",
+      showSpinner: false
+    },
+    StatusBar: {
+      style: 'LIGHT',
+      backgroundColor: '#ffffff'
+    }
+  }
+};
+
+export default config;
