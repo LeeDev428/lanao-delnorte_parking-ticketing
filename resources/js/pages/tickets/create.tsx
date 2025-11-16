47d283ce5eb6d9e99704317039063db92293a6a4
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-16 21:23:49 +0800

    Update create.tsx

--- resources/js/pages/tickets/create.tsx
+++ resources/js/pages/tickets/create.tsx
@@ -74,7 +74,7 @@ export default function NewTicket({ rateSettings, parkingZones }: NewTicketProps
         ]}>
             <Head title="New Ticket" />
             
-            <div className="max-w-4xl mx-auto p-3 sm:p-6">
+            <div className="max-w-7xl mx-auto p-3 sm:p-6">
                 {/* Header */}
                 <div className="bg-blue-600 text-white rounded-t-2xl p-4 sm:p-6">
                     <div className="flex items-center gap-2 sm:gap-3">
