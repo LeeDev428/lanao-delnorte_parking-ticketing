91adf774e43d1489c7b19f7a962b7969edc92302
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-16 21:12:10 +0800

    Update app-sidebar.tsx

--- resources/js/components/app-sidebar.tsx
+++ resources/js/components/app-sidebar.tsx
@@ -13,7 +13,7 @@ import {
 import { dashboard } from '@/routes';
 import { type NavItem } from '@/types';
 import { Link } from '@inertiajs/react';
-import { LayoutGrid, Ticket, History, PlusCircle } from 'lucide-react';
+import { LayoutGrid, Ticket, History, PlusCircle, Receipt } from 'lucide-react';
 import AppLogo from './app-logo';
 
 const mainNavItems: NavItem[] = [
@@ -36,6 +36,11 @@ const mainNavItems: NavItem[] = [
         title: 'History',
         href: '/tickets/history',
         icon: History,
+    },
+    {
+        title: 'My Remittance',
+        href: '/tickets/remittance',
+        icon: Receipt,
     },
 ];
 
