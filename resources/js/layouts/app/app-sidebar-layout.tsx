611ad3906a1204eae6c8ab70f040e94c882d3b1b
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-25 02:16:08 +0800

    Update app-sidebar-layout.tsx

--- resources/js/layouts/app/app-sidebar-layout.tsx
+++ resources/js/layouts/app/app-sidebar-layout.tsx
@@ -2,6 +2,7 @@ import { AppContent } from '@/components/app-content';
 import { AppShell } from '@/components/app-shell';
 import { AppSidebar } from '@/components/app-sidebar';
 import { AppSidebarHeader } from '@/components/app-sidebar-header';
+import { PrinterProvider } from '@/contexts/printer-context';
 import { type BreadcrumbItem } from '@/types';
 import { type PropsWithChildren } from 'react';
 
@@ -10,12 +11,14 @@ export default function AppSidebarLayout({
     breadcrumbs = [],
 }: PropsWithChildren<{ breadcrumbs?: BreadcrumbItem[] }>) {
     return (
-        <AppShell variant="sidebar">
-            <AppSidebar />
-            <AppContent variant="sidebar" className="overflow-x-hidden">
-                <AppSidebarHeader breadcrumbs={breadcrumbs} />
-                {children}
-            </AppContent>
-        </AppShell>
+        <PrinterProvider>
+            <AppShell variant="sidebar">
+                <AppSidebar />
+                <AppContent variant="sidebar" className="overflow-x-hidden">
+                    <AppSidebarHeader breadcrumbs={breadcrumbs} />
+                    {children}
+                </AppContent>
+            </AppShell>
+        </PrinterProvider>
     );
 }
