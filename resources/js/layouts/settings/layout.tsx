396a34467786356900e8e8171626bbc6866cb4d8
Lee Torres <leerafaelbtorres@iskolarngbayan.pup.edu.ph>
2025-11-30 16:47:11 +0800

    Update layout.tsx

--- resources/js/layouts/settings/layout.tsx
+++ resources/js/layouts/settings/layout.tsx
@@ -11,21 +11,21 @@ import { Link } from '@inertiajs/react';
 import { type PropsWithChildren } from 'react';
 
 const sidebarNavItems: NavItem[] = [
-    {
-        title: 'Profile',
-        href: edit(),
-        icon: null,
-    },
-    {
-        title: 'Password',
-        href: editPassword(),
-        icon: null,
-    },
-    {
-        title: 'Two-Factor Auth',
-        href: show(),
-        icon: null,
-    },
+    // {
+    //     title: 'Profile',
+    //     href: edit(),
+    //     icon: null,
+    // },
+    // {
+    //     title: 'Password',
+    //     href: editPassword(),
+    //     icon: null,
+    // },
+    // {
+    //     title: 'Two-Factor Auth',
+    //     href: show(),
+    //     icon: null,
+    // },
     {
         title: 'Appearance',
         href: editAppearance(),
